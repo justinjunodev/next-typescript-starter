63f7b5e83a87a1bf15287c3e19c41cf58e0e46c2
Justin Juno <50022106+justinjunodev@users.noreply.github.com>
2022-02-04 15:55:37 -0600

    add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Link from 'next/Link';
+import SEO from 'components/SEO';
+
+const PageNotFound: NextPage = () => {
+  return (
+    <div>
+      <SEO pageName="Page Not Found" />
+
+      <h1>Next + TypeScript Starter</h1>
+      <p>
+        Page not found. <Link href="/">Return home?</Link>
+      </p>
+    </div>
+  );
+};
+
+export default PageNotFound;
