3e0f448274ef543f14d659b357784c45a410df4c
Justin Juno <hello@justinjuno.dev>
2022-08-05 20:50:00 -0500

    simplify and remove second test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -8,13 +8,4 @@ test.describe('Home', () => {
   test('Document title includes page name', async ({ page }) => {
     await expect(page).toHaveTitle(/Home/);
   });
-
-  test('Link to github repo is valid', async ({ page }) => {
-    const link = page.locator('a', { hasText: 'View on GitHub' });
-    await expect(link).toBeVisible();
-    await expect(link).toHaveAttribute(
-      'href',
-      'https://github.com/justinjunodev/next-typescript-starter'
-    );
-  });
 });
